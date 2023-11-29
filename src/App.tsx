4752a74be905aef28889066020afedadcb938329
HariBhandari07 <bhandarihari07@gmail.com>
2023-11-29 11:24:47 +0545

    Feat: Create a basic text editor using draft.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { TextEditor } from "./components/TextEditor/TextEditor.tsx";
 
 function App() {
   return (
-    <>
-      Text Editor
-    </>
+    <div className='appContainer'>
+      <h3 className='title'>Draft Docs</h3>
+      <TextEditor />
+    </div>
   );
 }
 
